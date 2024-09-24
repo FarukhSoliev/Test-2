ee01f0798c87d21895c773e8e3de899840699c9f
Farukh <soliev_farux@mail.ru>
2024-09-24 14:25:24 +0300

    Test

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        int[] list1 = {1, 2, 3};
+        int[] list1 = {2, 2, 3};
         int[] list2 = {1, 2, 3};
         list2 = list1;
         list1[0] = 0; list1[1] = 1; list2[2] = 2;
